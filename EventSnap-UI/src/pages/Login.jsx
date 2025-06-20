450ef9ac987321e347ea1b7e7552d301fa65b3db
Praveen <praveengoud8722@gmail.com>
2025-06-20 23:43:21 +0530

    i have added login page text in Login.jsx

--- EventSnap-UI/src/pages/Login.jsx
+++ EventSnap-UI/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
 
   return (
     <Box sx={{ width: 300, mx: 'auto', mt: 10 }}>
-      <Typography variant="h5" mb={2}>Login</Typography>
+      <Typography variant="h5" mb={2}>Login page</Typography>
       <form onSubmit={handleLogin}>
         <TextField label="Email" fullWidth margin="normal" />
         <TextField label="Password" type="password" fullWidth margin="normal" />
