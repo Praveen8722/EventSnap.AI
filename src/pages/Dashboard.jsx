3a2364778669b19a221142ea60a3719d2f60b836
Praveen <praveengoud8722@gmail.com>
2025-06-20 23:23:39 +0530

    initial commit - gitignore file added

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import AppBarHeader from '../components/AppBarHeader';
+import EventTable from '../components/EventTable';
+
+export default function Dashboard() {
+  return (
+    <>
+      <AppBarHeader />
+      <EventTable />
+    </>
+  );
+}
---
+++ src/pages/Register.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField, Box, Typography } from '@mui/material';
+
+export default function Register() {
+  const navigate = useNavigate();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    navigate('/');
+  };
+
+  return (
+    <Box sx={{ width: 300, mx: 'auto', mt: 10 }}>
+      <Typography variant="h5" mb={2}>Register</Typography>
+      <form onSubmit={handleRegister}>
+        <TextField label="Email" fullWidth margin="normal" />
+        <TextField label="Password" type="password" fullWidth margin="normal" />
+        <Button type="submit" variant="contained" fullWidth>Register</Button>
+      </form>
+    </Box>
+  );
+}
